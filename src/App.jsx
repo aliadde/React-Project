3958d1e91b70a27bdaebbe83cc6656920aeabb1e
aliadde <aliadely1384@gmail.com>
2024-09-17 11:20:11 +0330

    we have done with title and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        id="container"
+        className=" m-10 p-8 grid-rows-3 grid justify-center  content-center items-center text-xl"
+      >
+        <div className="box">
+          <div>write your Todo List</div>
+          <div>To Do</div>
+        </div>
+        <div className="Add">
+          <label htmlFor="todo">Add :</label>
+          <input type="text" name="todo" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
